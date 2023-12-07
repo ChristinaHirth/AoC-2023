53982357315b9ee594d8511dd532bcc736c24092
christinahirth <christina.hirth@payfit.com>
2023-12-07 19:37:12 +0100

    day 7, part 2 done

--- day7/day7.js
+++ day7/day7.js
@@ -12,6 +12,8 @@ const {sortDesc} = require("../day5/day5")
  * @type {string}
  */
 const cardLabels = '23456789TJQKA'
+const cardLabels2 = 'J23456789TQKA'
+const joker = 'J';
 
 const cardLabelsList = () => Array.from(cardLabels);
 
@@ -26,27 +28,49 @@ const getLabels = hand => Array.from(hand);
 /**
  *
  * @param {string} hand
+ * @param withJoker
  * @return string
  */
-const getHandAsWeight = (hand) => {
+const getHandAsWeight = (hand, withJoker = false) => {
     const labels = getLabels(hand)
     const weights = labels.map(l => {
-        const index = cardLabels.indexOf(l) + 1
+        const index = (withJoker ? cardLabels2 : cardLabels).indexOf(l) + 1
         return (index < 10) ? '0' + index : '' + index
     })
     return weights.join('')
 }
 
-const occurrences = (handAsString, card) => Number(handAsString.split(card).length - 1)
-const allOccurrences = (hand) => {
-    const handAsString = hand.join('')
-    return hand.map(c => occurrences(handAsString, c))
+const occurrences = (handAsString, card) => {
+    // console.log(handAsString)
+    return Number(handAsString.split(card).length - 1);
+}
+const allOccurrences = (cards) => {
+    const handAsString = cards.join('')
+    return cards.map(c => occurrences(handAsString, c))
 }
 const maxOccurrence = (hand) => allOccurrences(hand).sort(sortDesc)[0]
 
 const numberOfPairs = hand => allOccurrences(hand).filter(o => o === 2).length / 2
 
 const isFullHouse = hand => allOccurrences(hand).filter(v => v === 2 || v === 3).length === 5
+
+const isFullHouseWithJoker = (cards, nrOfJoker) => {
+    const differentCards = cards.filter((value, index, self) => self.indexOf(value) === index)
+    return differentCards.length === 2 && nrOfJoker === 1;
+}
+
+const nrOfPairsWithJoker = (cards, nrOfJoker) => {
+    const differentCards = cards.filter((value, index, self) => self.indexOf(value) === index)
+    if ((differentCards.length + nrOfJoker) === 5) return onePair
+    if ((differentCards.length + nrOfJoker) === 4) return twoPairs
+    return 1
+}
+const fiveAKind = 7
+const fourAKind = 6
+const fullHouse = 5
+const threeAKind = 4
+const twoPairs = 3
+const onePair = 2
 
 /**
  *
@@ -57,14 +81,29 @@ const getHandType = (hand) => {
     const handAsCards = Array.from(hand)
     const nrSameCards = maxOccurrence(handAsCards)
 
-    if (nrSameCards === 5) return 7
-    if (nrSameCards === 4) return 6
-    if (isFullHouse(handAsCards)) return 5
-    if (nrSameCards === 3) return 4
-    if (numberOfPairs(handAsCards) === 2) return 3
-    if (numberOfPairs(handAsCards) === 1) return 2
+    if (nrSameCards === 5) return fiveAKind
+    if (nrSameCards === 4) return fourAKind // four
+    if (isFullHouse(handAsCards)) return fullHouse //full house
+    if (nrSameCards === 3) return threeAKind // three
+    if (numberOfPairs(handAsCards) === 2) return twoPairs //two pairs
+    if (numberOfPairs(handAsCards) === 1) return onePair //one pair
     return 1
+}
 
+const getHandType2 = hand => {
+    const nrJokers = occurrences(hand, joker)
+    if (nrJokers === 0) return getHandType(hand)
+
+    if (nrJokers > 3) return fiveAKind
+    const regex = new RegExp(joker, 'g')
+    const handWithoutJoker = hand.replace(regex, '')
+    const cardsButJoker = Array.from(handWithoutJoker);
+    const nrSameCards = maxOccurrence(cardsButJoker)
+    if (nrSameCards + nrJokers === 5) return fiveAKind
+    if (nrSameCards + nrJokers === 4) return fourAKind
+    if (isFullHouseWithJoker(cardsButJoker, nrJokers)) return fullHouse
+    if (nrSameCards + nrJokers === 3) return threeAKind // three
+    return nrOfPairsWithJoker(cardsButJoker, nrJokers)
 }
 
 const sortHands = (hands) => {
@@ -77,8 +116,22 @@ const sortHands = (hands) => {
         return getHandAsWeight(h1.hand) - getHandAsWeight(h2.hand)
     })
 }
+const sortHands2 = (hands) => {
+    return hands.map(h => {
+        return {bid: h.bid, hand: h.hand, handType: getHandType2(h.hand)}
+    }).sort((h1, h2) => {
+        const diff = h1.handType - h2.handType;
+        if (diff !== 0) return diff
+
+        return getHandAsWeight(h1.hand, true) - getHandAsWeight(h2.hand, true)
+    })
+}
 
 const totalWinnings = (hands) => sortHands(hands)
+    .map((h, i) => h.bid * (i + 1))
+    .reduce((acc, value) => acc + value, 0)
+
+const totalWinnings2 = (hands) => sortHands2(hands)
     .map((h, i) => h.bid * (i + 1))
     .reduce((acc, value) => acc + value, 0)
 
@@ -88,5 +141,9 @@ module.exports = {
     getHandAsWeight,
     getHandType,
     sortHands,
-    totalWinnings
+    totalWinnings,
+    getHandType2,
+    sortHands2,
+    totalWinnings2,
+    fiveAKind, fourAKind, fullHouse, threeAKind, twoPairs, onePair
 }
--- day7/day7.test.js
+++ day7/day7.test.js
@@ -1,7 +1,32 @@
 const assert = require('assert');
-const {allCards, getHandAsWeight, getHandType, sortHands, totalWinnings} = require("./day7");
+const {
+    allCards,
+    getHandAsWeight,
+    getHandType,
+    sortHands,
+    totalWinnings,
+    getHandType2,
+    sortHands2,
+    totalWinnings2, fullHouse, fourAKind, threeAKind, onePair, fiveAKind
+} = require("./day7");
 const input = require('./input')
 
+const newInput = [
+    {hand: '2345A', bid: 1}, {hand: 'Q2KJJ', bid: 13}, {hand: 'Q2Q2Q', bid: 19},
+    {hand: 'T3T3J', bid: 17}, {hand: 'T3Q33', bid: 11}, {hand: '2345J', bid: 3},
+    {hand: 'J345A', bid: 2}, {hand: '32T3K', bid: 5}, {hand: 'T55J5', bid: 29},
+    {hand: 'KK677', bid: 7}, {hand: 'KTJJT', bid: 34}, {hand: 'QQQJA', bid: 31},
+    {hand: 'JJJJJ', bid: 37},
+    {hand: 'JAAAA', bid: 43}, {hand: 'AAAAJ', bid: 59}, {hand: 'AAAAA', bid: 61},
+    {hand: '2AAAA', bid: 23}, {hand: '2JJJJ', bid: 53}, {hand: 'JJJJ2', bid: 41}]
+const hands = [
+    {hand: '32T3K', bid: 765},
+    {hand: 'T55J5', bid: 684},
+    {hand: 'KK677', bid: 28},
+    {hand: 'KTJJT', bid: 220},
+    {hand: 'QQQJA', bid: 483},
+
+]
 describe('Part 1', () => {
     it('should determine all cards with weight', () => {
         const cards = allCards()
@@ -25,14 +50,7 @@ describe('Part 1', () => {
     })
 
     it('should sort hands and get the total winnings', () => {
-        const hands = [
-            {hand: '32T3K', bid: 765},
-            {hand: 'T55J5', bid: 684},
-            {hand: 'KK677', bid: 28},
-            {hand: 'KTJJT', bid: 220},
-            {hand: 'QQQJA', bid: 483},
 
-        ]
         const sorted = sortHands(hands)
         assert.deepEqual(sorted, [
             {hand: '32T3K', handType: 2, bid: 765},
@@ -43,8 +61,45 @@ describe('Part 1', () => {
 
         assert.equal(totalWinnings(hands), 6440)
 
+        assert.equal(totalWinnings(newInput), 6592)
 
-        const clue = totalWinnings(input)
-        assert.equal(clue, 255048101)
+        //const clue = totalWinnings(input)
+
+        //assert.equal(clue, 255048101)
+    })
+})
+
+describe('part 2', () => {
+    it('should use jokers', () => {
+        assert.equal(getHandType2('QQJAA'), fullHouse)
+        assert.equal(getHandType2('QJJJA'), fourAKind)
+        assert.equal(getHandType2('KTJJT'), fourAKind)
+        assert.equal(getHandType2('Q2KJJ'), threeAKind)
+        assert.equal(getHandType2('T3T3J'), fullHouse)
+        assert.equal(getHandType2('7J78J'), fourAKind)
+        assert.equal(getHandType2('2345J'), onePair)
+        assert.equal(getHandType2('J345A'), onePair)
+        assert.equal(getHandType2('T55J5'), fourAKind)
+        assert.equal(getHandType2('QQQJA'), fourAKind)
+        assert.equal(getHandType2('JJJJ2'), fiveAKind)
+        assert.equal(getHandType2('JAAAA'), fiveAKind)
+
+
+        assert.deepEqual(sortHands2(hands), [
+            {hand: '32T3K', handType: 2, bid: 765},
+            {hand: 'KK677', handType: 3, bid: 28},
+            {hand: 'T55J5', handType: 6, bid: 684},
+            {hand: 'QQQJA', handType: 6, bid: 483},
+            {hand: 'KTJJT', handType: 6, bid: 220}]
+        )
+
+        assert.equal(totalWinnings2(hands), 5905)
+
+        const clue = totalWinnings2(newInput)
+        assert.equal(clue, 6839)
+
+        const cluePart2 = totalWinnings2(input)
+        assert.equal(cluePart2, 253718286)
+
     })
 })
